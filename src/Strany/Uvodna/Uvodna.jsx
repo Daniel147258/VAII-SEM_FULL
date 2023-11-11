37b231453fd2a27b71095e8bf61735bd731b6fe1
Daniel Dziaba <danieldziaba@gmail.com>
2023-11-11 15:20:46 +0100

    Pridanie menu

--- src/Strany/Uvodna/Uvodna.jsx
+++ src/Strany/Uvodna/Uvodna.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import PomocnyNavBar from '../../Komponenty/PomocnyNavBar/PomocnyNavBar'
 import Navbar from '../../Komponenty/Navbar/Navbar'
-
+import Menu from '../../Komponenty/Menu/Menu'
 const Uvodna = () => {
   return (
     <div>
         <PomocnyNavBar/>
         <Navbar/>
+        <Menu/>
     </div>
   )
 }
